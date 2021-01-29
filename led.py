9d6d66d85b53bbaf563352b6a9775cde92351ad9
ericaveltman <71352751+ericaveltman@users.noreply.github.com>
2021-01-29 18:42:53 -0500

    Update led.py
    Added comments

--- led.py
+++ led.py
@@ -4,28 +4,23 @@ import board
 import neopixel
 import time
 
-time.sleep(30)
+time.sleep(20) #wait for system to start up
 
 pixels = neopixel.NeoPixel(board.D21, 8, brightness=0.1, auto_write=False, pixel_order=neopixel.RGB)
+
 df = pd.read_csv("https://api.covidtracking.com/v1/states/ny/daily.csv")
 newcases_week = df.positiveIncrease[:8]
-#print(newcases_week)
+
 lednum = 0
 for i in range(7,0,-1):
     if newcases_week[i] > newcases_week[i-1]:
-        pixels[lednum] = (255, 0, 0)
+        pixels[lednum] = (255, 0, 0)  #glow red
     else:
-        pixels[lednum] = (0, 255, 0)
+        pixels[lednum] = (0, 255, 0)  #glow green
     lednum+=1
-
-    #while True:
         
 pixels.show()
-time.sleep(30)
+time.sleep(60)  #added this line so that the leds turn off after 60s b/c I didn't want them to keep running; can be altered or removed
 pixels.fill((0,0,0))
 pixels.show()
 
-'''except KeyboardInterrupt:
-    pixels.fill((0,0,0))
-    pixels.show()
-'''
